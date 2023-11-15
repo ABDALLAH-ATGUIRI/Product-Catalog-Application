bbeba24daadb522850d67ee1b410177db84a2ec7
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-15 15:47:16 +0100

    create category from command line

---
+++ app/Console/Commands/CreateCategory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\category;
+use Illuminate\Console\Command;
+
+class CreateCategory extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'category:create {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new category';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        category::create(['name' => $this->argument('name')]);
+
+        $this->info('Category created successfully!');
+    }
+}
