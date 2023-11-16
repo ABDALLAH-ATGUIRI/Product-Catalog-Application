670f6cfa9d4887e891d4c6e03e89113d936e74c4
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-16 10:39:48 +0100

    add update product action

---
+++ app/Actions/ProductActions/UpdateProductAction.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Actions\ProductActions;
+
+use App\Actions\GlobalActions\UploadImage;
+use App\Helpers\ImageHelper;
+use App\Models\Product;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class UpdateProductAction
+{
+    public function execute(array $data, int $id)
+    {
+        try {
+            $product = Product::find($id);
+
+            if (!$product) {
+                return response()->json([
+                    'message' => 'Product not found!'
+                ], 404);
+            }
+
+            $product->name = $data['name'];
+            $product->price = $data['price'];
+            $product->description = $data['description'];
+            $product->category_id = $data['category_id'];
+            $product->image = ImageHelper::upload($data['image']);
+
+            //Update Product
+            $product->save();
+
+            return response()->json([
+                'message' => 'Product successfully updated!'
+            ], 200);
+
+        } catch (\Exception $e) {
+            //throw $th;
+            return response()->json([
+                'message' => 'Something went really wrong!'
+            ], 500);
+        }
+    }
+}
