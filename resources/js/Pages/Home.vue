29d7bb5ba8508e43d9e7d6dbd28b34ae61ba5e8a
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-16 09:55:21 +0100

    create first interface

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <main role="main">
+        <search-bar></search-bar>
+        <div class="catalog">
+            <div v-for="item in 5" :key="item">
+                <product-card></product-card>
+            </div>
+        </div>
+        <pagination-bar
+            :items="['', '', '', '','']"
+            :itemsPerPage="10"
+        ></pagination-bar>
+    </main>
+</template>
+
+<script setup>
+import ProductCard from "../components/ProductCard.vue";
+import SearchBar from "../components/SearchBar.vue";
+import PaginationBar from "../components/PaginationBar.vue";
+</script>
+
+<style scoped>
+main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.catalog {
+    display: flex;
+    gap: 10px;
+}
+</style>
