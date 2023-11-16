d273ba7d03cd5af1b9ea304358834462a25e9fce
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-16 15:50:24 +0100

    recreate search bar

--- resources/js/components/SearchBar.vue
+++ resources/js/components/SearchBar.vue
@@ -1,64 +1,127 @@
 <template lang="">
-    <div class="search-bar">
-        <input type="text" class="search-input" placeholder="Search..." />
-        <select class="filter-select">
-            <option value="">Filter by Category</option>
-            <option value="category1">Category 1</option>
-            <option value="category2">Category 2</option>
-            <option value="category3">Category 3</option>
-        </select>
-        <select class="sort-select">
-            <option value="">Sort by</option>
-            <option value="name-asc">Name (A-Z)</option>
-            <option value="name-desc">Name (Z-A)</option>
-            <option value="price-asc">Price (Low to High)</option>
-            <option value="price-desc">Price (High to Low)</option>
-        </select>
+    <div id="app">
+        <div class="filter-bar">
+            <select v-model="filterForm.category_id">
+                <template v-for="category in categories">
+                    <optgroup
+                        v-if="category.parent_id === null"
+                        :label="category.name"
+                    >
+                        <template v-for="subCategory in categories">
+                            <option
+                                v-if="
+                                    subCategory.parent_id &&
+                                    subCategory.parent_id == category.id
+                                "
+                                :value="subCategory.id"
+                            >
+                                {{ subCategory.name }}
+                            </option>
+                        </template>
+                    </optgroup>
+                </template>
+            </select>
+
+            <input
+                v-model="filterForm.product_name"
+                type="text"
+                placeholder="Product Name"
+            />
+
+            <label for="minPrice">Min Price:</label>
+            <input
+                v-model.number="filterForm.min_prix"
+                type="number"
+                id="minPrice"
+                min="0"
+            />
+
+            <label for="maxPrice">Max Price:</label>
+            <input
+                v-model.number="filterForm.max_prix"
+                type="number"
+                id="maxPrice"
+                min="0"
+            />
+
+            <button @click="searchProducts">Search</button>
+        </div>
     </div>
 </template>
+<script setup>
+import { onMounted, ref } from "vue";
+
+const categories = ref([]);
+const filterForm = ref([]);
+
+onMounted(() => {
+    fetch("/api/categories")
+        .then((response) => response.json())
+        .then((data) => {
+            categories.value = data.categories;
+        })
+        .catch((error) => {
+            console.error("Error fetching categories:", error);
+        });
+});
+
+const searchProducts = () => {
+    console.log(filterForm.value)
+    fetch(`/api/product/search`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ search: filterForm.value }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error("Error fetching products:", error);
+        });
+};
+</script>
+
 <style scoped>
-.search-bar {
+.filter-bar {
+    background-color: #333;
+    color: white;
+    padding: 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    margin-bottom: 40px;
-    margin-top: 20px;
+    margin-bottom: 3rem;
 }
 
-.search-input {
-    flex-grow: 1;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+.filter-bar select,
+.filter-bar input {
+    margin-right: 10px;
+    padding: 5px;
+    font-size: 16px;
 }
 
-.filter-select,
-.sort-select {
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+.filter-bar button {
+    padding: 8px 15px;
+    font-size: 16px;
+    background-color: #4caf50;
+    color: white;
+    border: none;
+    cursor: pointer;
 }
 
 .product-list {
-    list-style: none;
-    padding: 0;
-    margin: 0;
+    margin: 20px;
+    padding: 10px;
+    background-color: white;
+    border-radius: 8px;
 }
 
 .product {
-    border: 1px solid #ccc;
-    margin-bottom: 10px;
+    border: 1px solid #ddd;
     padding: 10px;
-}
-
-.pagination {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
-
-.pagination-item {
-    margin-right: 5px;
+    margin-bottom: 10px;
+    border-radius: 4px;
 }
 </style>
