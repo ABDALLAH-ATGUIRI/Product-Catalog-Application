1bccd166f4a9005f1933156ad78ecc6dcd545112
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-16 10:19:05 +0100

    add categorycontroller methods with inertia

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,63 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Actions\CategoryActions\CreateCategoryAction;
+use App\Actions\CategoryActions\DeleteCategoryAction;
+use App\Actions\CategoryActions\ReadAllCategoriesAction;
+use App\Actions\CategoryActions\UpdateCategoryAction;
+use App\Http\Requests\CategoryRequest;
+use Inertia\Inertia;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(ReadAllCategoriesAction $readAllCategoriesAction)
     {
-        //
+        return Inertia::render('Categories/Index', ['categories' => $readAllCategoriesAction]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(CreateCategoryAction $createCategoryAction, CategoryRequest $categoryRequest)
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        return Inertia::render('Categories/create', ['categories' => $createCategoryAction($categoryRequest)]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateCategoryAction $updateCategoryAction, CategoryRequest $categoryRequest, string $id)
     {
-        //
+        return Inertia::render('Categories/update', ['categories' => $updateCategoryAction($categoryRequest, $id)]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(DeleteCategoryAction $deleteCategoryAction, string $id)
     {
-        //
+        return Inertia::render('Categories/delete', ['categories' => $deleteCategoryAction($id)]);
     }
 }
